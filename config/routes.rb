2a66c81cdfce3789d61372d340f2f8aa8d58663f
elmo0509 <89878165+elmo0509@users.noreply.github.com>
2021-11-16 06:06:04 +0000

    devise_admin設定作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  #管理者側はログイン機能のみ
+  #登録、変更は行わない
+  devise_for :admin, skip: [:registrations, :passwords], controllers:{
+    sessions: "admin/sessions"
+  }
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
