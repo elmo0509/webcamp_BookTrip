d14138cb509fbf3825bd2f389c90973abdde6a97
elmo0509 <89878165+elmo0509@users.noreply.github.com>
2021-11-16 23:42:48 +0000

    introduction作成

--- app/controllers/introductions_controller.rb
+++ app/controllers/introductions_controller.rb
@@ -1,2 +1,46 @@
 class IntroductionsController < ApplicationController
+
+  def new
+    @introduction = Introduction.new
+  end
+
+  def create
+    @introduction = Introduction.new(introduction_params)
+    @introduction.user_id = current_user.id
+    @introduction.save
+    redirect_to introductions_path
+  end
+
+  def show
+    @introduction = Introduction.find(params[:id])
+  end
+
+  def index
+    @introductions = Introduction.all
+  end
+
+  def edit
+    @introduction = Introduction.find(params[:id])
+  end
+
+  def update
+    @introduction = Introduction.find(params[:id])
+    if @introduction.update(introduction_params)
+      redirect_to introduction_path(@introduction.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @introduction = Introduction.find(params[:id])
+    @introduction.destroy
+    redirect_to introductions_path
+  end
+
+  private
+  def introduction_params
+    params.require(:introduction).permit(:place, :author, :title, :address_prefecture, :address_after_prefecture, :address_building_name, :introduction)
+  end
+
 end
