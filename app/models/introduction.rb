c8848b4694552340e6266aacf1688196a3070738
elmo0509 <89878165+elmo0509@users.noreply.github.com>
2021-11-16 07:30:52 +0000

    モデルの関連付け

--- app/models/introduction.rb
+++ app/models/introduction.rb
@@ -1,2 +1,7 @@
 class Introduction < ApplicationRecord
+  belongs_to :user
+  has_many :favorites, dependent: :destroy
+  has_many :tag_maps, dependent: :destroy
+  has_many :tags, through: :tag_maps
+  attachment :image
 end
