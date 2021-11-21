96a52ef3ba9e3083b4fc44fa8e279f038decafe8
elmo0509 <89878165+elmo0509@users.noreply.github.com>
2021-11-21 03:01:58 +0000

    ランキング作成

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,7 @@
 class HomesController < ApplicationController
 
   def top
+    @all_ranks = Introduction.find(Favorite.group(:introduction_id).order('count(introduction_id) desc').limit(5).pluck(:introduction_id))
   end
 
   def about
