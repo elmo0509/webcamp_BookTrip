59911164dc98dd1503e0fc1dd4c960158af4e862
elmo0509 <89878165+elmo0509@users.noreply.github.com>
2021-11-27 22:51:31 +0000

    Admin topレイアウト編集

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,6 +2,6 @@ class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!
 
   def top
-    @introductions = Introduction.all
+    @introductions = Introduction.all.order(created_at: :desc)
   end
 end
