e09401e86520edcb4d589aa9e1bddd588b3bb1ed
elmo0509 <89878165+elmo0509@users.noreply.github.com>
2021-11-28 09:37:01 +0000

    Refileエラー修正

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '5839074e03d0f2eee9e0a4c076fa1f9ec3f9d15ad8d3e99e76597f8f33a1bc5485048ea6d26e42a5e460e646b1252f26df0efc6fdd28f7d1026bdb42ee86cdb4'
